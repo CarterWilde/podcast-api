5033cfe3878f5a310eb3ae3538a4bc8e41fd6a3f
CarterWilde <carterjwilde@gmail.com>
2020-01-05 20:37:03 -0700

    Basic Request Caching

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
 
 const PORT = process.env.PORT;
 const PODCAST_ID = process.env.PODCAST_ID;
@@ -14,10 +15,35 @@ app.get('/', (req, res) => {
     res.redirect("/episodes");
 });
 
-app.get('/episodes', async(req, res) => {
-    const response = await axios.get(`https://buzzsprout.com/api/${PODCAST_ID}/episodes.json`, { headers: { 'Authorization': `Token token=${API_TOKEN}` } });
-    res.set('Access-Control-Allow-Origin', FRONT_END_ENDPOINT);
-    res.json(response.data);
-})
+let etag = "";
+app.get('/episodes', async (req, res) => {
+    const cachePath = './episodes.cache.json';
+    try {
+        const response = await axios.get(`https://buzzsprout.com/api/${PODCAST_ID}/episodes.json`, {
+            headers: {
+                'Authorization': `Token token=${API_TOKEN}`,
+                'If-None-Match': etag
+            }
+        });
+        etag = response.headers.etag;
+        res.set('Access-Control-Allow-Origin', FRONT_END_ENDPOINT);
+        res.json(response.data);
+        if (response.status === 200) {
+            fs.writeFile(cachePath, JSON.stringify(response.data), () => {
+                console.log(`Saved request '/episodes' to file '${cachePath}'`);
+            });
+        }
+    } catch (error) {
+        if(error.response.status === 304){
+            fs.readFile(cachePath, 'utf8', (error, data) => {
+                console.log(`Sending cached data from file ${cachePath}`);
+                res.json(JSON.parse(data));
+                if (error) {
+                    console.log(error);
+                }
+            });
+        }
+    }
+});
 
 app.listen(PORT, console.log(`API listening on port ${PORT}!`));
